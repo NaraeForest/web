33e982126730fd3bc5bcc0d0a7a4f7206f0fd87d
baikdk <bdk0206@naver.com>
2024-11-13 15:50:29 +0900

    로그인 페이지 구현
    - 로그인 페이지 (구글, 카카오, 네이버)를 구현함

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        naverColor: '#03C75A',
+        kakaoColor: '#FEE500'
+      }
+    },
   },
   plugins: [
     require("tailwind-scrollbar-hide"), // 플러그인 추가
