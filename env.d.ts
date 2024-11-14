4af734ddae01ec144f3f227e9b3c304823058b75
narumir <narumir@narumir.io>
2024-11-14 05:52:52 +0000

    add social login

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_API_URL: string;
+    NEXT_PUBLIC_NAVER_CLIENT_ID: string;
+    NEXT_PUBLIC_NAVER_REDIRECT_URL: string;
+    NEXT_PUBLIC_KAKAO_CLIENT_ID: string;
+    NEXT_PUBLIC_KAKAO_REDIRECT_URL: string;
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
+    NEXT_PUBLIC_GOOGLE_REDIRECT_URL: string;
+  }
+}
