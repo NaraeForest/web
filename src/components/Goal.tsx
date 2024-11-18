87bddcdecdf0d7f645ec786af29eba974a8c7106
baikdk <bdk0206@naver.com>
2024-11-18 10:21:32 +0900

    메인페이지 스크롤바 비활성화
    - tailwind.config를 이용하여 메인페이지 스크롤바 비활성화하기

--- src/components/Goal.tsx
+++ src/components/Goal.tsx
@@ -24,7 +24,7 @@ export default function main(){
         </div>
   
         <h2 className="text-2xl font-semibold mt-2 mb-2 ml-6">Sub Goals</h2>
-        <div className="flex-1 px-4 pb-14 overflow-y-auto">
+        <div className="flex-1 px-4 pb-14 overflow-y-auto scrollbar-hide">
           <div className="sub-goals-container bg-gray-100 p-4 rounded-lg shadow-md">
             {subGoals.map((goal, index) => (
               <Link href="/subgoalviewer" key={index}>
