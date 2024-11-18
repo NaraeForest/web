f8db22da7be9452b80015c7cac3ac23bfe8aca39
baikdk <bdk0206@naver.com>
2024-11-18 10:14:03 +0900

    메인페이지 메인골 색깔 수정
    - 회색에서 흰색으로

--- src/components/main-goal.tsx
+++ src/components/main-goal.tsx
@@ -9,7 +9,7 @@ type MainGoalProps = {
 export default function MainGoal({ name, progress }: MainGoalProps){
     return(
       <Link href="/maingoalviewer">
-        <div className="goalCard bg-gray-100 p-4 rounded-lg shadow-md">
+        <div className="goalCard bg-white p-4 rounded-lg shadow-md">
           <h1 className="text-lg font-semibold mb-2">{name}</h1>
           <div className="progressBar w-full bg-gray-300 rounded-full h-2 mb-2">
             <div
