988293812cc4290083dac85c16ea9cbe16f2ea6d
narumir <narumir@narumir.io>
2024-11-24 13:11:40 +0000

    update bottom navigation

--- src/components/bottom-navigation-bar.tsx
+++ src/components/bottom-navigation-bar.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
 import Link from "next/link";
+import {
+  useRouter,
+} from "next/router";
 
 export function BattomNavigationBar() {
+  const router = useRouter();
   return (
     <div className="fixed bottom-0 w-full h-16 border-t border-[#D9D9D9] flex justify-evenly items-center text-[0.625rem] bg-white z-50">
       <Link href={"/"} className="flex flex-col items-center">
-        <Image src={"/home.svg"} width={24} height={24} alt="go to home" />
+        <Image
+          src={router.pathname === "/" ? "/home-full.svg" : "/home-line.svg"}
+          width={24}
+          height={24}
+          alt="go to home"
+        />
         Home
       </Link>
       <Link href={"/feeds"} className="flex flex-col items-center">
@@ -16,8 +25,16 @@ export function BattomNavigationBar() {
         <Image src={"/signal.svg"} width={24} height={24} alt="go to feeds" />
         Trends
       </Link>
-      <Link href={"/mypage"} className="flex flex-col items-center">
-        <Image src={"/user.svg"} width={24} height={24} alt="go to feeds" />
+      <Link
+        href={"/mypage"}
+        className="flex flex-col items-center"
+      >
+        <Image
+          src={router.pathname === "/mypage" ? "/user-full.svg" : "/user-line.svg"}
+          width={24}
+          height={24}
+          alt="go to feeds"
+        />
         MyPage
       </Link>
     </div>
