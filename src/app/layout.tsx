0531b66321bbd1b14fd410ead49865ba22b32abc
narumir <narumir@narumir.io>
2024-12-23 10:54:53 +0000

    add pwa configure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, viewport-fit=cover" />
+        <meta
+          name="viewport"
+          content="initial-scale=1, viewport-fit=cover"
+        />
       </head>
       <body className={`${pretendard.variable} font-pretendard text-[#333333]`}>
         {children}
